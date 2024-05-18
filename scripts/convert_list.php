48972a6a44c954d7eec4517609ada29d274e71e3
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-05-18 17:35:32 +0200

    Error message first

--- scripts/convert_list.php
+++ scripts/convert_list.php
@@ -72,13 +72,14 @@
     document.getElementById("add").addEventListener("submit", function(event) {
       var speciesSelect1 = document.getElementById("species1");
       var speciesSelect2 = document.getElementById("species2");
-      var selectedSpecies1 = speciesSelect1.options[speciesSelect1.selectedIndex].value;
-      var selectedSpecies2 = speciesSelect2.options[speciesSelect2.selectedIndex].value;
-      document.getElementById("species").value = selectedSpecies1 + ";" + selectedSpecies2;
       if (speciesSelect1.selectedIndex < 0 || speciesSelect2.selectedIndex < 0) {
         alert("Please select a species from both lists.");
         document.querySelector('.views').style.opacity = 1;
         event.preventDefault();
+      } else {
+        var selectedSpecies1 = speciesSelect1.options[speciesSelect1.selectedIndex].value;
+        var selectedSpecies2 = speciesSelect2.options[speciesSelect2.selectedIndex].value;
+        document.getElementById("species").value = selectedSpecies1 + ";" + selectedSpecies2;
       }
     });
 
