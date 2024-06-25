b6ea2747007386ff6556276de8442df23398420d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-06-25 12:22:13 +0200

    Sci_name in italics

--- scripts/overview.php
+++ scripts/overview.php
@@ -130,7 +130,7 @@ if(isset($_GET['ajax_detections']) && $_GET['ajax_detections'] == "true" && isse
                   <input type="hidden" name="view" value="Species Stats">
                   <button type="submit" name="species" value="<?php echo $mostrecent['Com_Name'];?>"><?php echo $mostrecent['Com_Name'];?></button>
                   <br>
-                  <?php echo $mostrecent['Sci_Name'];?>
+                  <i><?php echo $mostrecent['Sci_Name'];?></i>
                   <a href="<?php $info_url = get_info_url($mostrecent['Sci_Name']); $url = $info_url['URL']; echo $url ?>" target="_blank">
                   <img style="width: unset !important; display: inline; height: 1em; cursor: pointer;" title="Info" src="images/info.png" width="25"></a>
                   <a href="https://wikipedia.org/wiki/<?php echo $sciname;?>" target="_blank"><img style="width: unset !important; display: inline; height: 1em; cursor: pointer;" title="Wikipedia" src="images/wiki.png" width="25"></a>
