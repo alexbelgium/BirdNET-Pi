a034da22eac79cb3db99b99e9492816546a30bc9
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-07-10 14:42:23 +0200

    Several code

--- scripts/observation_upload.php
+++ scripts/observation_upload.php
@@ -4,12 +4,19 @@
 $_GET   = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
 $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-error_reporting(E_ERROR);
+//error_reporting(E_ERROR);
+error_reporting(E_ALL);
 ini_set('display_errors',1);
 require_once 'scripts/common.php';
-$home = get_home();
+//$home = get_home();
 $config = get_config();
 $user = get_user();
+$CLIENT_ID = 'birdnet-pi';
+$OBS_EMAIL = $config['OBS_EMAIL'];
+$OBS_PASS= $config['OBS_PASSWORD'];
+$OBS_SITE = $config['OBS_SITE'];
+$filename = filter_input(INPUT_GET, 'filename', FILTER_SANITIZE_STRING);
+$uuid = filter_input(INPUT_GET, 'uuid', FILTER_SANITIZE_STRING);
 
 // List of functions
 //getOBSToken
@@ -18,19 +25,35 @@ $user = get_user();
 //postOBS
 //updateOBS
 
-$filename = $_GET['filename'];
+//function getUUIDFromFile($filename) {
+//    $observations_filename = $home."/BirdNET-Pi/scripts/observations_uploaded_list.txt";
+//    if (!file_exists($observations_filename) || filesize($observations_filename) == 0) {
+//      file_put_contents($observations_filename, "# List of uploaded species to observations and UUID\n");
+//    }
 
-function getOBSToken() {
-    $CLIENT_ID = getenv('CLIENT_ID');
-    $MAIL = getenv('MAIL');
-    $PASSWORD = getenv('PASSWORD');
+//    $file = fopen($observations_filename, 'r'); // replace 'yourfile.txt' with your actual file path
+//    while (($line = fgets($file)) !== false) {
+//        list($uuid, $file_name) = explode(';', trim($line));
+//        if ($file_name == $filename) {
+//            fclose($file);
+//            return $uuid;
+//        }
+//    }
+//
+//    fclose($file);
+//    return '';
+//}
 
+function getOBSToken($filename) {
+    global $filename;
+    global $OBS_SITE, $CLIENT_ID, $OBS_EMAIL, $OBS_PASS;
+    
     $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, 'https://observation.org/api/v1/oauth2/token/');
+    
+    curl_setopt($ch, CURLOPT_URL, 'https://' . $OBS_SITE . '/api/v1/oauth2/token/');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('client_id' => $CLIENT_ID, 'grant_type' => 'password', 'email' => $MAIL, 'password' => $PASSWORD)));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('client_id' => $CLIENT_ID, 'grant_type' => 'password', 'email' => $OBS_EMAIL, 'password' => $OBS_PASS)));
 
     $headers = array();
     $headers[] = 'Content-Type: application/x-www-form-urlencoded';
@@ -49,34 +72,32 @@ function getOBSToken() {
 }
 
 function getObservationData() {
+    global $filename;
     $db = new SQLite3('./scripts/birds.db', SQLITE3_OPEN_READONLY);
     $db->busyTimeout(5000);
-    $statement2 = $db->prepare("SELECT * FROM detections where File_name == \"$filename\" ORDER BY Date DESC, Time DESC");
+    $statement2 = $db->prepare('SELECT * FROM detections WHERE File_name = :filename ORDER BY Date DESC, Time DESC');
+    $statement2->bindValue(':filename', $filename);
     ensure_db_ok($statement2);
     $result2 = $statement2->execute();
     while($results=$result2->fetchArray(SQLITE3_ASSOC))
     {
-        $sciname = $results['Sci_Name'];
-        $comname = $results['Com_Name'];
-        $confidence = $results['Confidence'];
+        //$sciname = $results['Sci_Name'];
+        //$comname = $results['Com_Name'];
+        //$confidence = $results['Confidence'];
         $filename = $results['File_Name'];
         $date = $results['Date'];
         $time = $results['Time'];
-        $week = $results['Week'];
+        //$week = $results['Week'];
         $latitude = $results['Lat'];
         $longitude = $results['Lon'];
-        $cutoff = $results['Cutoff'];
-        $sens = $results['Sens'];
-        $overlap = $results['Overlap'];
+        //$cutoff = $results['Cutoff'];
+        //$sens = $results['Sens'];
+        //$overlap = $results['Overlap'];
     }
-
-    // Define variables
-    $OBS_UUID = "<replace_with_uuid>"; // replace with your uuid
-    $server = "observation.org";
 
     // Prepare new observation data
     $OBS_DATA = array(
-        'species' => '<replace_with_species>', // replace with your species
+        'species' => '<replace_with_species>', // needed from observation.org dev
         'date' => $date,
         'time' => $time,
         'point' => "POINT($latitude $longitude)",
@@ -86,30 +107,32 @@ function getObservationData() {
     return $OBS_DATA;
 }
 
-function getObservationSound() {
+function getObservationSound($OBS_DATA) {
+    global $filename;
     if (file_exists($filename)) {
         $file_parts = pathinfo($filename);
     if ($file_parts['extension'] == 'mp3' || $file_parts['extension'] == 'wav') {
         $cfile = new CURLFile($filename, 'audio/mpeg', basename($filename));
         $OBS_DATA['upload_sounds'] = $cfile;
-        return $OBS_DATA;
     } else {
         echo '<script type="text/javascript">alert("The file is not a MP3 or WAV file.");</script>';
     }
     } else {
         echo '<script type="text/javascript">alert("The file does not exist.");</script>';
     }
+    return $OBS_DATA;
 }
 
-function postOBS {
-    $headers = array('Authorization: Bearer ' . getOBSToken());
+function postOBS($OBSTOKEN, $OBS_DATA) {
+    global $server;
+    $headers = array('Authorization: Bearer ' . $OBSTOKEN);
     $url = "https:" .$server . "/api/v1/observations/create/";
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $OBS_DATA);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        $OBS_HEADERS,
+        implode("\r\n", $headers),
         "Content-Type: multipart/form-data"
     ));
 
@@ -122,9 +145,13 @@ function postOBS {
     curl_close($ch);
 }
 
+# Generate token
 $OBSTOKEN = getOBSToken();
-$data = getObservationData();
-$data = getObservationSound();
-postOBS();
+# Get data from species
+$OBS_DATA = getObservationData();
+# Append sound if relevant
+$OBS_DATA = getObservationSound($OBS_DATA);
+# Post observation
+postOBS($OBSTOKEN,$OBS_DATA);
 
 ?>
