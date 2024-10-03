e00152f1b699d17e41dad2557adb34b14c6fdca5
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-10-03 11:42:44 +0200

    Add SNR

--- scripts/utils/reporting.py
+++ scripts/utils/reporting.py
@@ -107,7 +107,7 @@ def summary(file: ParseFileName, detection: Detection):
     s = (f'{file.date};{file.time};{detection.scientific_name};{detection.common_name};'
          f'{detection.confidence};'
          f'{conf["LATITUDE"]};{conf["LONGITUDE"]};{conf["CONFIDENCE"]};{file.week};{conf["SENSITIVITY"]};'
-         f'{conf["OVERLAP"]}')
+         f'{conf["OVERLAP"]};{detection.snr}')
     return s
 
 
