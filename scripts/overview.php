fbb2477d0fed0ce822a9d03b509fdcaebf278167
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-10-30 17:34:26 +0100

    Update overview.php

--- scripts/overview.php
+++ scripts/overview.php
@@ -438,7 +438,7 @@ if ($newspeciescount > 0): ?>
         <?php endif; ?>
     </div>
 <?php endif; ?>
-<div class="chart">
+<div class="chart" style="visibility: hidden;">
 <?php
 $refresh = $config['RECORDING_LENGTH'];
 $dividedrefresh = $refresh/4;
@@ -456,9 +456,13 @@ if (file_exists($interactivechart_path)) {
 }
 ?>
 <script>
-    if (window.innerWidth <= 800) {
-        document.querySelector('.chart').innerHTML = '<img id="chart" src="Charts/<?php echo $chart; ?>?nocache=<?php echo $time; ?>">';
-    }
+    document.addEventListener("DOMContentLoaded", function() {
+        const chartContainer = document.querySelector('.chart');
+        if (window.innerWidth <= 800) {
+            chartContainer.innerHTML = '<img id="chart" src="Charts/<?php echo $chart; ?>?nocache=<?php echo $time; ?>">';
+        }
+        chartContainer.style.visibility = 'visible';
+    });
 </script>
 </div>
 
