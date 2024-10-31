615ee3c0036830d09b8f1e66fc342b47bb29b88c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-10-31 08:40:23 +0100

    Improve filtering appearance

--- scripts/utils/interactive_plot.py
+++ scripts/utils/interactive_plot.py
@@ -228,10 +228,10 @@ def create_plotly_heatmap(df_birds, now):
     <div class='chart-container' style='position: relative; width: 80%; margin: 0 auto;'>
         <div style='position: absolute; top: 10px; left: 10px; z-index: 10;'>
             <input type='text' id='birdSearch' placeholder='Search...'
-            style='padding: 5px; font-size: 14px; background-color: #ddd; color: #333; border: none;
-            border-radius: 3px; opacity: 0.8; width: 150px;' />
-            <button id='filterButton' style='margin-left: 5px; padding: 5px; font-size: 14px; background-color: #ddd;
-            color: #333; border: none; border-radius: 3px; opacity: 0.8;'>OK</button>
+            style='padding: 5px; font-size: 14px; background-color: rgba(255, 255, 255, 0.5); color: #333; border: none;
+            border-radius: 3px; width: 150px;' />
+            <button id='filterButton' style='margin-left: 5px; padding: 5px; font-size: 14px; background-color: rgba(255, 255, 255, 0.5);
+            color: #333; border: none; border-radius: 3px;'>OK</button>
         </div>
         {fig.to_html(
             include_plotlyjs='cdn',
