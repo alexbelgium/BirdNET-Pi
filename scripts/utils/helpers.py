4b06393f695dee275872017bd7b684766eac3631
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-11-10 09:55:35 +0100

    Update helpers.py

--- scripts/utils/helpers.py
+++ scripts/utils/helpers.py
@@ -42,7 +42,7 @@ def get_settings(settings_path='/etc/birdnet/birdnet.conf', force_reload=False):
 
 
 class Detection:
-    def __init__(self, start_time, stop_time, species, confidence, snr):
+    def __init__(self, start_time, stop_time, species, confidence, snr, quality):
         self.start = float(start_time)
         self.stop = float(stop_time)
         self.confidence = round(float(confidence), 4)
@@ -52,7 +52,8 @@ class Detection:
         self.common_name = species.split('_')[1]
         self.common_name_safe = self.common_name.replace("'", "").replace(" ", "_")
         self.file_name_extr = None
-        self.snr = str(snr)
+        self.snr = snr
+        self.quality = quality
 
 
 class ParseFileName:
