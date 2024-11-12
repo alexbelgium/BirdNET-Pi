ede21f417a78ecb65b6fab662a31ddf96f31eacb
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-11-12 09:23:59 +0100

    Update overview.php

--- scripts/overview.php
+++ scripts/overview.php
@@ -409,7 +409,7 @@ function display_species($species_list, $title, $show_last_seen=false) {
                         }
                     ?>
                     <tr class="relative" id="<?php echo $iterations; ?>">
-                        <td><?php echo nl2br(htmlspecialchars($last_seen_text)); ?><br></td>
+                        <td><?php echo $last_seen_text; ?><br></td>
                         <td><?php if (!empty($image_url)): ?>
                           <img onclick='setModalText(<?php echo $iterations; ?>,"<?php echo urlencode($image[2]); ?>", "<?php echo $image[3]; ?>", "<?php echo $image[4]; ?>", "<?php echo $image[1]; ?>", "<?php echo $image[5]; ?>")' src="<?php echo htmlspecialchars($image_url); ?>" style="height: 50px; width: 50px; border-radius: 5px; cursor: pointer;" class="img1" title="Image from Flickr" />
                         <?php endif; ?></td>
