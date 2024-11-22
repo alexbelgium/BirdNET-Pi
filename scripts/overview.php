8873de5fa08d91bf5d84a5b9f66b63336307748d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-11-22 21:59:54 +0100

    Update overview.php

--- scripts/overview.php
+++ scripts/overview.php
@@ -405,9 +405,9 @@ function display_species($species_list, $title, $show_last_seen=false) {
                             $days_ago = $todaytable['DaysAgo'];
                             if ($days_ago > 30) {
                                 $months_ago = floor($days_ago / 30);
-                                $last_seen_text = "<br><i>Last seen: {$months_ago}mo ago</i>";
+                                $last_seen_text = "<br><i>Last: {$months_ago}mo ago</i>";
                             } else {
-                                $last_seen_text = "<br><i>Last seen: {$days_ago}d ago</i>";
+                                $last_seen_text = "<br><i>Last: {$days_ago}d ago</i>";
                             }
                             
                         }
@@ -435,7 +435,7 @@ function display_species($species_list, $title, $show_last_seen=false) {
                                     </i>
                             </form></div>
                         </td>
-                        <td style="white-space: nowrap;"><b>Max Confidence: <?php echo round($todaytable['Confidence'] * 100 ) . '%</b>'; echo $last_seen_text; echo $time_occurrence_text; ?><br></td>
+                        <td style="white-space: nowrap;"><b>Max Conf.: <?php echo round($todaytable['Confidence'] * 100 ) . '%</b>'; echo $last_seen_text; echo $time_occurrence_text; ?><br></td>
                     </tr>
                     <?php endforeach; ?>
                 </table>
