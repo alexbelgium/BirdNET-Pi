4b0552e42dc39a734154a63b21f3b63a21129203
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-11-27 14:45:32 +0100

    Update overview.php

--- scripts/overview.php
+++ scripts/overview.php
@@ -434,7 +434,7 @@ function display_species($species_list, $title, $show_last_seen=false) {
                                     </i>
                             </form></div>
                         </td>
-                        <td style="white-space: nowrap;">Max Confidence: <?php echo round($todaytable['Confidence'] * 100 ) . '%' . "<br>{$todaytable['Time']}" . $occurrence_text . $last_seen_text; ?><br></td>
+                        <td style="white-space: nowrap;"><span class="text left">Max Confidence: </span><?php echo round($todaytable['Confidence'] * 100 ) . '%' . "<br><span class="text left">First detection: </span>{$todaytable['Time']}" . $occurrence_text . '<br>' . '<span class="text left">Last seen since: </span>' . $last_seen_text; ?><br></td>
                     </tr>
                     <?php endforeach; ?>
                 </table>
