6c0cb54f3228316c052a458167cecc7a35e7fffd
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-01-19 14:12:03 +0100

    Align disk cleaning with disk_species_clean.sh

--- scripts/disk_check.sh
+++ scripts/disk_check.sh
@@ -7,37 +7,14 @@ used="$(df -h ${EXTRACTED} | tail -n1 | awk '{print $5}')"
 if [ "${used//%}" -ge 95 ]; then
 
   case $FULL_DISK in
-    purge) echo "Removing oldest data"
-        cd ${EXTRACTED}/By_Date/
-        curl localhost/views.php?view=Species%20Stats &>/dev/null
-        if ! grep -qxFe \#\#start $HOME/BirdNET-Pi/scripts/disk_check_exclude.txt; then
-            exit
-        fi
-        filestodelete=$(($(find ${EXTRACTED}/By_Date/* -type f | wc -l) / $(find ${EXTRACTED}/By_Date/* -maxdepth 0 -type d | wc -l)))
-        iter=0
-        for i in */*/*; do
-            if [ $iter -ge $filestodelete ]; then
-                break
-            fi
-            if ! grep -qxFe "$i" $HOME/BirdNET-Pi/scripts/disk_check_exclude.txt; then
-                rm "$i"
-            fi
-            ((iter++))
-        done
-        find ~/BirdSongs/ -type d -empty -mtime +90 -delete
-        find ${EXTRACTED}/By_Date/ -empty -type d -delete;;
-
-       #rm -drfv "$(find ${EXTRACTED}/By_Date/* -maxdepth 1 -type d -prune \
-        # | sort -r | tail -n1)";;
-    keep) echo "Stopping Core Services"
-       /usr/local/bin/stop_core_services.sh;;
-  esac
-fi
-sleep 1
-if [ "${used//%}" -ge 95 ]; then
-  case $FULL_DISK in
-    purge) echo "Removing more data"
-       rm -rfv ${PROCESSED}/*;;
+    purge) echo "Removing data to stay below threshold"
+        max_files_species="1000"
+        safe_purge_threshold="$((95 * 9 / 10))"
+        while [ "$(df -h "${EXTRACTED}" | tail -n1 | awk '{print $5}' | tr -d '%')" -ge "$safe_purge_threshold" ]; do
+            ./disk_species_clean.sh "$max_files_species"
+            max_files_species=$((max_files_species * 9 / 10))
+        done;;
+      
     keep) echo "Stopping Core Services"
        /usr/local/bin/stop_core_services.sh;;
   esac
