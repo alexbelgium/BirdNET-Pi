a606540290a26dfd37b160bf4f934276108a0e1a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-01-23 23:24:27 +0100

    Update todays_detections.php

--- scripts/todays_detections.php
+++ scripts/todays_detections.php
@@ -245,7 +245,7 @@ if(isset($_GET['ajax_detections']) && $_GET['ajax_detections'] == "true"  ) {
           <a href="https://wikipedia.org/wiki/<?php echo $sciname;?>" target="_blank"><img style=";cursor:pointer;float:unset;display:inline" title="Wikipedia" src="images/wiki.png" width="20"></a>
           <img style=";cursor:pointer;float:unset;display:inline" title="View species stats" onclick="generateMiniGraph(this, '<?php echo $comnamegraph; ?>')" width=20 src="images/chart.svg"><br>
           <b>Confidence:</b> <?php echo round((float)round($todaytable['Confidence'],2) * 100 ) . '%';?><br></div><br>
-          <div class='custom-audio-player' data-audio-src='<?php echo $filename; ?>' data-image-src='<?php echo $filename.".png";?>'></div>
+          <div class='custom-audio-player' data-audio-src="<?php echo $filename; ?>" data-image-src="<?php echo $filename.".png";?>"></div>
           </td>
         <?php } else { //legacy mode ?>
           <tr class="relative" id="<?php echo $iterations; ?>">
