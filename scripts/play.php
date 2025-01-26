929d56936570ef443e34e981c3b1b709191950ad
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-01-26 10:37:01 +0100

    Update play.php

--- scripts/play.php
+++ scripts/play.php
@@ -717,7 +717,7 @@ echo "><br><i>$sciname</i></span><br>
     }
     $iter++;
 
-    if($num_rows < 41) {
+    if($num_rows < 100) {
       $imageelem = "<div class='custom-audio-player' data-audio-src=\"$filename\" data-image-src=\"$filename_png\"></div>";
     } else {
       $imageelem = "<a href=\"$filename\"><img src=\"$filename_png\"></a>";
