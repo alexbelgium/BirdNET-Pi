061420263048dd48a6f172d9f47cc29314acf576
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-03-31 11:14:18 +0200

    Align

--- scripts/play.php
+++ scripts/play.php
@@ -652,14 +652,12 @@ $name = htmlspecialchars_decode($_GET['species'], ENT_QUOTES);
 $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 40;
 if(isset($_SESSION['date'])) {
   $date = $_SESSION['date'];
-  $countQuery = $db->prepare("SELECT COUNT(*) as total FROM detections WHERE Com_Name == \"$name\" AND Date == \"$date\"");
   if(isset($_GET['sort']) && $_GET['sort'] == "confidence") {
     $statement2 = $db->prepare("SELECT * FROM detections where Com_Name == \"$name\" AND Date == \"$date\" ORDER BY Confidence DESC");
   } else {
     $statement2 = $db->prepare("SELECT * FROM detections where Com_Name == \"$name\" AND Date == \"$date\" ORDER BY Time DESC");
   }
 } else {
-  $countQuery = $db->prepare("SELECT COUNT(*) as total FROM detections WHERE Com_Name == \"$name\"");
   if(isset($_GET['sort']) && $_GET['sort'] == "confidence") {
     $statement2 = $db->prepare("SELECT * FROM detections where Com_Name == \"$name\" ORDER BY Confidence DESC");
   } else {
@@ -668,7 +666,6 @@ if(isset($_SESSION['date'])) {
 }
 ensure_db_ok($statement2);
 $result2 = $statement2->execute();
-$totalCount = $countQuery->execute()->fetchArray(SQLITE3_ASSOC)['total'];
 $num_rows = 0;
 while ($result2->fetchArray(SQLITE3_ASSOC)) {
     $num_rows++;
@@ -768,7 +765,7 @@ echo "><br><i>$sciname</i></span><br>
 
   if($iter == 0){ echo "<tr><td><b>No recordings were found.</b><br><br><span style='font-size:medium'>They may have been deleted to make space for new recordings. You can prevent this from happening in the future by clicking the <img src='images/unlock.svg' style='width:20px'> icon in the top right of a recording.<br>You can also modify this behavior globally under \"Full Disk Behavior\" <a href='views.php?view=Advanced'>here.</a></span></td></tr>";}echo "</table>";}
 
-  if($limit < $totalCount) {
+  if (!($iter < $limit)) {
     echo "<div style='text-align:center'>";
     echo "<form action='views.php' method='GET' style='display:inline'>";
     echo "<input type='hidden' name='view' value='Recordings'>";
