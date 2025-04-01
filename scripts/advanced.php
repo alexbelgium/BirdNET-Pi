209bea6017beb232dc41a7defec68a253f2e4cee
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-01 17:54:42 +0200

    Disk usage summary

--- scripts/advanced.php
+++ scripts/advanced.php
@@ -5,6 +5,7 @@ error_reporting(E_ERROR);
 require_once "scripts/common.php";
 $home = get_home();
 $config = get_config();
+$user = get_user();
 
 ensure_authenticated();
 
@@ -135,6 +136,14 @@ if(isset($_GET['submit'])) {
     if (strcmp($purge_threshold, $config['PURGE_THRESHOLD']) !== 0) {
         $contents = preg_replace("/PURGE_THRESHOLD=.*/", "PURGE_THRESHOLD=$purge_threshold", $contents);
     }
+}
+
+if (isset($_GET['run_species_count'])) {
+  echo "<script>";
+  $output = shell_exec("sudo -u $user ".$home."/BirdNET-Pi/scripts/disk_species_count.sh 2>&1");
+  $escaped_output = htmlspecialchars($output, ENT_QUOTES | ENT_SUBSTITUTE);
+  echo "alert(`$escaped_output`);";
+  echo "</script>";
 }
 
 if (isset($_GET["max_files_species"])) {
@@ -315,6 +324,8 @@ $newconfig = get_config();
       </td></tr><tr><td>
       Note only the spectrogram and audio files are deleted, the obsevation data remains in the database.
       The files protected through the "lock" icon are also not affected.
+      <br>
+      <button type="submit" name="run_species_count" value="1"><i>[Click here for disk usage summary]</i></button>
       </td></tr></table><br>
       <table class="settingstable"><tr><td>
 
