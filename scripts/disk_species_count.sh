b0840262ea242e504577f051b4ffdc00b6fcbeb5
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-06 09:48:11 +0200

    Shorten message for single alert

--- scripts/disk_species_count.sh
+++ scripts/disk_species_count.sh
@@ -59,7 +59,7 @@ done <<<"$sanitized_names"
 
 # Build final output
 {
-    echo "BirdSongs stored on your drive. This number is higher than the MAX_FILE_SPECIES (${MAX_FILE_SPECIES:-1000}) as files from the last 7 days are protected, as well as files specifically notified in the disk_check_exclude.txt"
+    echo "BirdSongs stored on your drive. This value is higher than MAX_FILE_SPECIES (${MAX_FILE_SPECIES:-1000}) as includes protected files"
     echo " "
     echo "Location : $base_dir: "
     echo "Free space    : $(df -h "$base_dir" | awk 'NR==2 {print $4}' | sed 's/G/ GB/; s/M/ MB/; s/K/ KB/')"
