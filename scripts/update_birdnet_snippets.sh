37ee37e17816b04308a9868bae34e11415e2fdfb
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-24 18:32:32 +0200

    Install bottle

--- scripts/update_birdnet_snippets.sh
+++ scripts/update_birdnet_snippets.sh
@@ -153,6 +153,8 @@ version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import seaborn; print(seaborn
 [[ $version != "0.13.2" ]] && sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install seaborn==0.13.2
 version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import suntime; print(suntime.__version__)")
 [[ $version != "1.3.2" ]] && sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install suntime==1.3.2
+version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import bottle; print(bottle.__version__)" 2>/dev/null || echo "0")
+echo "$version" | grep -q "0.12.25" && sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install bottle==0.12.25
 
 PY_VERSION=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import sys; print(f'{sys.version_info[0]}{sys.version_info[1]}')")
 tf_version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import tflite_runtime; print(tflite_runtime.__version__)")
