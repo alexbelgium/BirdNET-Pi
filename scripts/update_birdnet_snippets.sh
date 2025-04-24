3dc382c427880fd1a44604a49a4ddeb2372d7dce
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-24 22:33:48 +0200

    Install bottle

--- scripts/update_birdnet_snippets.sh
+++ scripts/update_birdnet_snippets.sh
@@ -145,16 +145,16 @@ if ! which inotifywait &>/dev/null;then
   apt-get -y install inotify-tools
 fi
 
-apprise_version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import apprise; print(apprise.__version__)")
+apprise_version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import apprise; print(apprise.__version__)" 2>/dev/null || echo "0")
 [[ $apprise_version != "1.9.0" ]] && sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install apprise==1.9.0
-version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import streamlit; print(streamlit.__version__)")
+version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import streamlit; print(streamlit.__version__)" 2>/dev/null || echo "0")
 [[ $version != "1.44.0" ]] && sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install streamlit==1.44.0
-version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import seaborn; print(seaborn.__version__)")
+version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import seaborn; print(seaborn.__version__)" 2>/dev/null || echo "0")
 [[ $version != "0.13.2" ]] && sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install seaborn==0.13.2
-version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import suntime; print(suntime.__version__)")
+version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import suntime; print(suntime.__version__)" 2>/dev/null || echo "0")
 [[ $version != "1.3.2" ]] && sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install suntime==1.3.2
 version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import bottle; print(bottle.__version__)" 2>/dev/null || echo "0")
-echo "$version" | grep -q "0.12.25" && sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install bottle==0.12.25
+[[ $version != "0.12.25" ]] && sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install bottle==0.12.25
 
 PY_VERSION=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import sys; print(f'{sys.version_info[0]}{sys.version_info[1]}')")
 tf_version=$($HOME/BirdNET-Pi/birdnet/bin/python3 -c "import tflite_runtime; print(tflite_runtime.__version__)")
