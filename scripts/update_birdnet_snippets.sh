2d586c8e1dfa03e72e6bfba602c4bb006695581d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-08-02 18:09:20 +0200

    Add soundfile

--- scripts/update_birdnet_snippets.sh
+++ scripts/update_birdnet_snippets.sh
@@ -140,7 +140,7 @@ if [ "$PY_VERSION" == 39 ] && [ "$tf_version" != "2.11.0" ] || [ "$PY_VERSION" !
   sudo_with_user $HOME/BirdNET-Pi/birdnet/bin/pip3 install $HOME/BirdNET-Pi/$WHL pandas librosa matplotlib
 fi
 
-ensure_python_package inotify inotify
+ensure_python_package inotify soundfile
 
 if ! which inotifywait &>/dev/null;then
   ensure_apt_updated
